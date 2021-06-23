a7fdaa0f5abb4144c2a455f0ca11369e02ecbb5c
David Patrick <the.great.vampire.slayer@gmail.com>
2021-06-23 16:22:18 -0400

    Added JS code for 4 nav bar buttons, changed <ul> to <div> for drop-down buttons

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ function HomeButton () {
     document.getElementById("home-drop-down").classList.toggle("show");
 }
 
+function NewsButton () {
+    document.getElementById("news-drop-down").classList.toggle("show");
+}
+
+function ContactButton () {
+    document.getElementById("contact-drop-down").classList.toggle("show");
+}
+
+function AboutButton () {
+    document.getElementById("about-drop-down").classList.toggle("show");
+}
 
